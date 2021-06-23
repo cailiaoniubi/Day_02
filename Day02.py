f6547c9c07eeec45dcabe824f6b6bc73a18118d3
Jian <jian.hu@tum.de>
2021-06-23 14:50:14 +0200

    beginning of day 2

--- Day02.py
+++ Day02.py
@@ -8,3 +8,18 @@ df = pd.DataFrame.from_dict(d, orient="index")
 pd.DataFrame()
 
 df.head()
+
+# x= "90"
+#
+# y= 100
+# z= x+y
+
+# int("9.0")
+
+# try except example
+
+try:
+    average = sum(a_list)/len(a_list)
+except (ZerodivsionEroor,ValueError):
+    print(a_list)
+    average = none
